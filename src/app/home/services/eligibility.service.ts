c2edc339514443ffb256b79f39dd2e0a907cc72c
ferid romdane <marzouki.taz@gmail.com>
2024-05-07 03:50:13 +0100

    second

--- src/app/home/services/eligibility.service.ts
+++ src/app/home/services/eligibility.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EligibilityService {
+  private baseUrl="http://localhost:8083/eligibilite"
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+  getAllElig(): Observable<any[]> {
+    const token = localStorage.getItem('Token');
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + token);
+    return this.http.get<any[]>(this.baseUrl,{ headers });
+  }
+
+  saveEvent(elig: any): Observable<any> {
+    const token = localStorage.getItem('Token');
+        const headers = new HttpHeaders().set('Authorization', 'Bearer ' + token);
+    return this.http.post<any>(this.baseUrl, elig,{headers});
+  }
+
 }
